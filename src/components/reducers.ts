f68b47bd86f938980d58a60f4e9d9968d97414fa
striped-s0cks <yanka.savchenko@gmail.com>
2020-11-16 21:07:59 -0800

    Add reducers

--- src/components/reducers.ts
+++ src/components/reducers.ts
@@ -1,13 +1,17 @@
 import { combineReducers } from "redux";
-import { actionTypes, CreateUserAction } from "./actions";
+import { actionTypes, SetUserAction } from "./actions";
 import { User } from "./types";
 
-function user(state = {} as User, action: CreateUserAction): User {
-    if (action.type === actionTypes.CreateUser) {
+function user(state = {} as User, action: SetUserAction): User {
+    if (action.type === actionTypes.SetUser) {
         return action.user;
     }
 
     return state;
+}
+
+export interface StoreState {
+    user: User;
 }
 
 export const reducers = combineReducers({
