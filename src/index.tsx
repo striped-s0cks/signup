c21d06f2686bcc938b98c44798bac81ceb264dec
striped-s0cks <yanka.savchenko@gmail.com>
2020-11-16 21:10:21 -0800

    Set up the env

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Redirect, Route } from 'react-router-dom';
-import { createStore } from 'redux';
-import { reducers } from './components/reducers';
-import { SignUpPageContainer } from './components/SignUpPageContainer';
+import { App } from './App';
 
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
-        <BrowserRouter>
-            <Redirect from='/' to='signup' />
-            <Route path='signup' component={SignUpPageContainer} />
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
